88023ca8681848d40c7c9cb4f3652683b3fb283c
leo <leo89420@yahoo.com.tw>
2020-12-19 14:15:58 +0800

    視窗測試ok

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,5 +1,24 @@
-public class GameClient {
-    public static void main(String[] args) {
-        System.out.println("111");
+import javax.swing.*;
+import java.awt.*;
+public class GameClient extends JComponent {
+        private int screenWidth;
+        private int screenHeight;
+    GameClient(){
+        this.setPreferredSize(new Dimension(800,600));
+
+    }
+
+    GameClient(int screenWidth,int screenHeight ){
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+
+        this.setPreferredSize(new Dimension(screenWidth , screenHeight));
+    }
+
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+        g.drawImage(new ImageIcon("assets/images/itankD.png").getImage(),
+                100,100,null);
     }
 }
